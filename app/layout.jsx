326a92f6b9f7cfaf128782ad5cfd6c706ce47022
Fuadmikayil <mikayilov2006@gmail.com>
2025-06-25 23:12:17 +0400

    generateMetadata edit text

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,7 @@
 import "./globals.css";
 import supabase from "../lib/supabase"; // Supabase client üçün yolu düzgün qeyd edin
 
-// Metadata obyektini asinxron funksiya ilə əvəz edirik
 export async function generateMetadata() {
-  // Supabase Storage-dən logonun URL-ni götürürük
-  // Bura öz bucket və fayl adınızı yazın
   const { data } = supabase
     .storage
     .from('coffee-images') // Sizin bucket adınız
